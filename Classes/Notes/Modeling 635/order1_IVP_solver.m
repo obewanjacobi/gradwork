482e03f98b834ba877e5f114936f976e85fbf9da
obewanjacobi <obewanjacobi@aim.com>
2017-10-04 00:22:34 -0400

    a few edits made, and a couple homework problems done. lots more to go

--- Classes/Notes/Modeling 635/order1_IVP_solver.m
+++ Classes/Notes/Modeling 635/order1_IVP_solver.m
@@ -1,10 +1,11 @@
 syms x(t);
 
 %%Input the 1st order DE here
-ode = diff(x,t) == x+1-t;
+ode = diff(x,t) == -x^3;
 
 %%Input initial condition here
-cond = x(0) == 1;
+x_0 = sym('x_0')
+cond = x(0) == x_0;
 
 xSol(t) = dsolve(ode,cond);
 xSol = simplify(xSol)
