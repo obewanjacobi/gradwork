7291c12804e33fa8c90bf372e32bbc8f2bcef7ec
Jacob Townson <obewanjacobi@aim.com>
2017-10-05 16:15:14 -0400

    more homework to do. notes added

--- Classes/Notes/Modeling 635/order1_IVP_solver.m
+++ Classes/Notes/Modeling 635/order1_IVP_solver.m
@@ -4,10 +4,10 @@ c_1 = sym('c_1');
 c_2 = sym('c_2');
 
 %%Input the 1st order DE here
-ode = diff(x,t) == 2*x + (c_1 * exp(-t))^2;
+ode = diff(x,t) == 2*x;
 
 %%Input initial condition here
-cond = x(0) == x_0;
+cond = x(0) == 0;
 
 xSol(t) = dsolve(ode,cond);
 xSol = simplify(xSol)
