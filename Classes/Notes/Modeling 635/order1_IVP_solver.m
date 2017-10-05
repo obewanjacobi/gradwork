0f887ba60c9ad6d908d52200e8990210238980aa
obewanjacobi <obewanjacobi@aim.com>
2017-10-04 23:35:27 -0400

    a little bit of work done, stats notes edited, and updated a bit of the matlab de solver code

--- Classes/Notes/Modeling 635/order1_IVP_solver.m
+++ Classes/Notes/Modeling 635/order1_IVP_solver.m
@@ -1,10 +1,12 @@
 syms x(t);
+x_0 = sym('x_0');
+c_1 = sym('c_1');
+c_2 = sym('c_2');
 
 %%Input the 1st order DE here
-ode = diff(x,t) == -x^3;
+ode = diff(x,t) == 2*x + (c_1 * exp(-t))^2;
 
 %%Input initial condition here
-x_0 = sym('x_0')
 cond = x(0) == x_0;
 
 xSol(t) = dsolve(ode,cond);
