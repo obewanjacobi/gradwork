f06fe693fc13b8215ed02a460b796a080e5d8ab6
obewanjacobi <obewanjacobi@aim.com>
2017-10-13 03:28:12 -0400

    take home exams suck...sometimes, this time it did

--- Classes/Notes/Modeling 635/order1_IVP_solver.m
+++ Classes/Notes/Modeling 635/order1_IVP_solver.m
@@ -6,10 +6,10 @@ syms c_3;
 c = [c_1;c_2;c_3];
 
 %%Input the 1st order DE here
-ode = diff(x,t) == -x+(c_1*exp(-t))^2;
+ode = diff(x,t) == 2*x-5*(c_1*exp(-t));
 
 %%Input initial condition here
-cond = x(0) == c_3;
+cond = x(0) == c_1^3;
 
 xSol(t) = dsolve(ode,cond);
 xSol = simplify(xSol)
--- Classes/Notes/Modeling 635/order2_IVP_solver.m
+++ Classes/Notes/Modeling 635/order2_IVP_solver.m
@@ -2,11 +2,11 @@ syms x(t)
 Dx = diff(x);
 
 %%Input 2nd order DE here
-ode = diff(x,t,2) == cos(2*t)-x; 
+ode = diff(x,t,2) == sin(3*t)-2*Dx-2*x; 
 
 %%Input initial conditions
-cond1 = x(0) == 1;
-cond2 = Dx(0) == 0;
+cond1 = x(0) == 0;
+cond2 = Dx(0) == 1;
 
 conds = [cond1 cond2];
 xSol(t) = dsolve(ode,conds);
