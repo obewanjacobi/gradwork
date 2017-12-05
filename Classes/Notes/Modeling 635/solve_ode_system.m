68ddbb7a0d88c28d44b3819fcaa116e599e613ae
Jacob Townson <obewanjacobi@aim.com>
2017-12-05 13:34:36 -0500

    more final problems solved. only 2 halves of problems away from being finished.

--- Classes/Notes/Modeling 635/solve_ode_system.m
+++ Classes/Notes/Modeling 635/solve_ode_system.m
@@ -1,6 +1,6 @@
 syms x(t) y(t) c_1 c_2 c_3;
-ode1 = diff(x) == (c_3*x+x^2)*cos(2*y);
-ode2 = diff(y) == 1- 2*c_3 *cos(y)*sin(y);
+ode1 = diff(x) == x*(c_3-x^2);
+ode2 = diff(y) == 1;
 odes = [ode1; ode2];
 cond1 = x(0) == c_1;
 cond2 = y(0) == c_2;
